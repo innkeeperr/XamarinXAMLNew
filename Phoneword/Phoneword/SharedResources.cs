ccd74d330eab26a02a2a6ae1a7f7af87d593d328
innkeeperr <39014566+innkeeperr@users.noreply.github.com>
2020-05-18 18:31:51 +0200

    Fixed  code, main commit

---
+++ Phoneword/Phoneword/SharedResources.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public static class SharedResources
+    {
+        public static Color ButtonBkColor
+        {
+            get { return Color.FromRgb(0xbb, 0x5a, 0); }
+        }
+
+        public static Color ButtonTextColor
+        {
+            get { return Color.White; }
+        }
+    }
+}
